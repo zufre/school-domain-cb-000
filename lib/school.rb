ef7dc2c480f29e058166b3d7427c2e6ae7c6922e
Cedric David <cedric.david@btopenworld.com>
2018-09-03 05:28:54 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,32 +1,3 @@
 class School
 
-  attr_reader :name, :roster
-
-  def initialize(name)
-    @name = name
-    @roster = {}
-  end
-
-  def roster
-    @roster
-  end
-
-  def add_student(name, grade)
-    if @roster[grade] != nil
-      @roster[grade] << name
-    else
-      @roster[grade] = []
-      @roster[grade] << name
-    end
-  end
-
-  def grade(grade)
-    @roster[grade]
-  end
-
-  def sort
-    self.roster.each do |grade, students|
-      students.sort!
-    end
-  end
 end
